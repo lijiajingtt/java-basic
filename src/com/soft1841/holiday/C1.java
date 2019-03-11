43111300bf3ce5b6c609058d8fbbe9fc7e48608f
lijiajingtt <lijiajingtt@gmail.com>
2019-03-11 22:17:03 +0800

    第二天小打卡

---
+++ src/com/soft1841/holiday/C1.java
@@ -0,0 +1,15 @@
+package com.soft1841.holiday;
+
+ class C1 {
+     public static void main(String[] args) {
+         double double1 = 23.4;
+         double double2 = 35.1;
+         if (double2 >= double1){
+             System.out.println(double1);
+             System.out.println(double2);
+         }else{
+             System.out.println(double2);
+             System.out.println(double1);
+         }
+     }
+}
